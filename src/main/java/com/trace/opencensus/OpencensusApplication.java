e118d82d36dfa670a6ae7ad59a4dc1fadde86073
Avinash Mathias <avinash.mathias@niveussolutions.com>
2022-02-15 22:43:35 +0530

    Added beans for stackDriverTracing

--- src/main/java/com/trace/opencensus/OpencensusApplication.java
+++ src/main/java/com/trace/opencensus/OpencensusApplication.java
@@ -1,7 +1,12 @@
 package com.trace.opencensus;
 
+import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
+import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class OpencensusApplication {
@@ -10,4 +15,9 @@ public class OpencensusApplication {
 		SpringApplication.run(OpencensusApplication.class, args);
 	}
 
+	@Bean
+	public static void createAndRegisterGoogleCloudPlatform() throws IOException {
+		StackdriverTraceExporter.createAndRegister(
+				StackdriverTraceConfiguration.builder().build());
+	}
 }
--- src/main/java/com/trace/opencensus/controller/Controller.java
+++ src/main/java/com/trace/opencensus/controller/Controller.java
@@ -1,14 +1,20 @@
 package com.trace.opencensus.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class controller {
+public class Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
+
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello(){
+        log.info("Inside the hello controller class of OpenCensus Consumer application");
         return ResponseEntity.ok("Hello");
     }
 }
