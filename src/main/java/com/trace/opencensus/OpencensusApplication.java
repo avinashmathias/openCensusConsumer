f4cc8016d3bf1f1758f2a77e3379a9153c97f6ce
Avinash Mathias <avinash.mathias@niveussolutions.com>
2022-02-15 21:03:00 +0530

    initial commit

---
+++ src/main/java/com/trace/opencensus/OpencensusApplication.java
@@ -0,0 +1,13 @@
+package com.trace.opencensus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencensusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpencensusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trace/opencensus/OpencensusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trace.opencensus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencensusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
