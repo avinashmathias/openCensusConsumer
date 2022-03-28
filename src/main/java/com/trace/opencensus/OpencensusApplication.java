ae85107fe341f5181ac185e9a75984d707060315
Avinash Mathias <avinash.mathias@niveussolutions.com>
2022-03-28 10:32:45 +0530

    Some config

--- src/main/java/com/trace/opencensus/OpencensusApplication.java
+++ src/main/java/com/trace/opencensus/OpencensusApplication.java
@@ -1,13 +1,10 @@
 package com.trace.opencensus;
 
+import com.google.cloud.opentelemetry.trace.TraceExporter;
 import com.trace.opencensus.controller.Controller;
-import io.opencensus.common.Scope;
-import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
-import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
-import io.opencensus.trace.*;
-import io.opencensus.trace.config.TraceConfig;
-import io.opencensus.trace.config.TraceParams;
-import io.opencensus.trace.samplers.Samplers;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -15,86 +12,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class OpencensusApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(Controller.class);
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
 
-	private static final Tracer tracer = Tracing.getTracer();
+	TraceExporter traceExporter = TraceExporter.createWithDefaultConfiguration();
 
+	public OpencensusApplication() throws IOException {
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(OpencensusApplication.class, args);
+        SpringApplication.run(OpencensusApplication.class, args);
+
+    }
+
+    @Bean
+	public void setTraceProvider(){
+		OpenTelemetrySdk.builder()
+				.setTracerProvider(
+						SdkTracerProvider.builder()
+								.addSpanProcessor(BatchSpanProcessor.builder(traceExporter).build())
+								.build())
+				.buildAndRegisterGlobal();
 	}
 
-	@Bean
-	public static void createAndRegisterGoogleCloudPlatform() throws IOException {
-		log.info("Inside createAndRegisterGoogleCloudPlatform method of application class");
-		StackdriverTraceExporter.createAndRegister(
-				StackdriverTraceConfiguration.builder().build());
-	}
-
-//	@Bean
-//	public static void doWorkFullSampled() {
-//		try (Scope ss =
-//					 tracer
-//							 .spanBuilder("MyChildWorkSpan")
-//							 .setSampler(Samplers.alwaysSample())
-//							 .startScopedSpan()){
-//			tracer.getCurrentSpan().addAnnotation("Tracer:Setting up full span");
-//		}
-//	}
-
-	@Bean
-	public static void initialWork(){
-		TraceConfig traceConfig = Tracing.getTraceConfig();
-		TraceParams activeTraceParams = traceConfig.getActiveTraceParams();
-		traceConfig.updateActiveTraceParams(activeTraceParams.toBuilder().setSampler(Samplers.alwaysSample()).build());
-
-		// 3. Get the global singleton Tracer object.
-		Tracer tracer = Tracing.getTracer();
-
-		// 4. Create a scoped span, a scoped span will automatically end when closed.
-		// It implements AutoClosable, so it'll be closed when the try block ends.
-		try (Scope scope = tracer.spanBuilder("main").startScopedSpan()) {
-			System.out.println("About to do some busy work...");
-			for (int i = 0; i < 10; i++) {
-				doWork(i);
-			}
-		}
-
-		// 5. Gracefully shutdown the exporter, so that it'll flush queued traces to Zipkin.
-		Tracing.getExportComponent().shutdown();
-	}
-
-	private static void doWork(int i) {
-		// 6. Get the global singleton Tracer object.
-		Tracer tracer = Tracing.getTracer();
-
-		// 7. Start another span. If another span was already started, it'll use that span as the parent span.
-		// In this example, the main method already started a span, so that'll be the parent span, and this will be
-		// a child span.
-		try (Scope scope = tracer.spanBuilder("doWork").startScopedSpan()) {
-			// Simulate some work.
-			Span span = tracer.getCurrentSpan();
-
-			try {
-				System.out.println("doing busy work");
-				Thread.sleep(100L);
-			}
-			catch (InterruptedException e) {
-				// 6. Set status upon error
-				span.setStatus(Status.INTERNAL.withDescription(e.toString()));
-			}
-
-			// 7. Annotate our span to capture metadata about our operation
-			Map<String, AttributeValue> attributes = new HashMap<>();
-			attributes.put("use", AttributeValue.stringAttributeValue("demo"));
-			span.addAnnotation("Invoking doWork", attributes);
-		}
-	}
 
 }
--- src/main/java/com/trace/opencensus/controller/Controller.java
+++ src/main/java/com/trace/opencensus/controller/Controller.java
@@ -1,25 +1,106 @@
 package com.trace.opencensus.controller;
 
-import io.opencensus.trace.Tracer;
-import io.opencensus.trace.Tracing;
+import io.opencensus.common.Scope;
+import io.opencensus.trace.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class Controller {
 
     private static final Logger log = LoggerFactory.getLogger(Controller.class);
 
-    private static final Tracer tracer = Tracing.getTracer();
+//    private static final Tracer tracer = Tracing.getTracer();
 
+    WebClient client = WebClient.create();
+//
+//    @GetMapping("/hello")
+//    public ResponseEntity<String> hello(){
+//        log.info("Inside hello method of controller class");
+////        tracer.getCurrentSpan().addAnnotation("Tracer:Inside hello method of controller class");
+//
+//        Tracer tracer = Tracing.getTracer();
+//
+//        // 7. Start another span. If another span was already started, it'll use that span as the parent span.
+//        // In this example, the main method already started a span, so that'll be the parent span, and this will be
+//        // a child span.
+//        try (Scope scope = tracer.spanBuilder("doWork").startScopedSpan()) {
+//            // Simulate some work.
+//            Span span = tracer.getCurrentSpan();
+//
+//            try {
+//                System.out.println("doing busy work");
+//                Thread.sleep(100L);
+//            }
+//            catch (InterruptedException e) {
+//                // 6. Set status upon error
+//                span.setStatus(Status.INTERNAL.withDescription(e.toString()));
+//            }
+//
+//            // 7. Annotate our span to capture metadata about our operation
+//            Map<String, AttributeValue> attributes = new HashMap<>();
+//            attributes.put("use", AttributeValue.stringAttributeValue("demo"));
+//            span.addAnnotation("Invoking doWork", attributes);
+//        }
+//
+//        return ResponseEntity.ok("Hello");
+//    }
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello(){
-        log.info("Inside hello method of controller class");
-        tracer.getCurrentSpan().addAnnotation("Tracer:Inside hello method of controller class");
-        return ResponseEntity.ok("Hello");
+//    @GetMapping("consume")
+//    public ResponseEntity<?> consume(){
+//        Boolean success = true;
+//        Mono<Boolean> response = client.get()
+//                .uri("http://localhost:8082/generate")
+//                .exchangeToMono(result ->  result.bodyToMono(Boolean.class));
+//        Mono<Boolean> response = client.get()
+//                .uri("http://localhost:8082/generate")
+//                .exchangeToMono(result ->  {
+//                    if(result.statusCode().is2xxSuccessful())
+//                        return Mono.just(true);
+//                    else
+//                        return Mono.just(false);
+//                });
+//        return ResponseEntity.ok(response.block());
+//    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> fetch(){
+        getRestSampleResponse();
+        getFailureResponse();
+        postUnsuccessfulLogin();
+        return ResponseEntity.ok("Hello World");
+    }
+
+    private void postUnsuccessfulLogin() {
+        HashMap<String, String> request = new HashMap<>();
+        request.put("email", "sydney@fife");
+        String sample = client.post()
+                .uri("https://reqres.in/api/users/23")
+                .body(Mono.just(request), HashMap.class)
+                .exchangeToMono(result -> result.bodyToMono(String.class)).block();
+        System.out.println("failure login:"+sample);
+    }
+
+    private void getFailureResponse() {
+        String sample = client.get()
+                .uri("https://reqres.in/api/users/23")
+                .exchangeToMono(result -> result.bodyToMono(String.class)).block();
+        System.out.println("failure:"+sample);
+    }
+
+    private void getRestSampleResponse() {
+        String sample = client.get()
+                .uri("https://reqres.in/api/users?page=2")
+                .exchangeToMono(result -> result.bodyToMono(String.class)).block();
+        System.out.println("rest-api"+sample);
     }
 }
